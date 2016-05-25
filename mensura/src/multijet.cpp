7b1fa2914de97a2bd2ad9b00bb7a08dc7780487d
Andrey Popov <rainfinder@gmail.com>
2016-05-25 12:55:45 +0200

    Update input files, PU profiles, and luminosities
    Since brilws currently cannot handle prescaled paths, use integrated
    luminosity and pile-up profiles for unprescaled selection everywhere.
    This is wrong, of course.

--- mensura/src/multijet.cpp
+++ mensura/src/multijet.cpp
@@ -57,39 +57,43 @@ int main(int argc, char **argv)
     
     // Input datasets
     list<Dataset> datasets;
-    string const datasetsDir("/gridgroup/cms/popov/Analyses/JetMET/2016.04.11_Grid-campaign/"
+    string const datasetsDir("/gridgroup/cms/popov/Analyses/JetMET/2016.05.21_Grid-campaign-80X/"
       "Results/");
     
     if (dataGroup == DatasetGroup::Data)
     {
         datasets.emplace_back(Dataset({Dataset::Process::ppData, Dataset::Process::pp13TeV},
           Dataset::Generator::Nature, Dataset::ShowerGenerator::Nature));
-        datasets.back().AddFile(datasetsDir + "JetHT-Run2015*.root");
+        datasets.back().AddFile(datasetsDir + "JetHT-Run2016*.root");
     }
     else
     {
         datasets.emplace_back(Dataset(Dataset::Process::QCD, Dataset::Generator::MadGraph,
           Dataset::ShowerGenerator::Pythia));
-        datasets.back().AddFile(datasetsDir + "QCD-Ht-100-200-mg_3.1.1_Kah.root",
-          27540000, 82095800);
-        datasets.back().AddFile(datasetsDir + "QCD-Ht-200-300-mg_3.1.1_ilS.root",
-          1717000, 18784379);
-        datasets.back().AddFile(datasetsDir + "QCD-Ht-300-500-mg_3.1.1_UpJ_p*.root",
-          351300, 16909004);
-        datasets.back().AddFile(datasetsDir + "QCD-Ht-500-700-mg_3.1.1_XWW_p*.root",
-          31630, 19665695);
-        datasets.back().AddFile(datasetsDir + "QCD-Ht-700-1000-mg_3.1.1_mtk_p*.root",
-          6802, 13801981);
-        datasets.back().AddFile(datasetsDir + "QCD-Ht-1000-1500-mg_3.1.1_MoZ.root",
-          1206, 5049267 + 10144378);
-        datasets.back().AddFile(datasetsDir + "QCD-Ht-1000-1500-mg_ext1_3.1.1_pTT_p*.root",
-          1206, 5049267 + 10144378);
-        datasets.back().AddFile(datasetsDir + "QCD-Ht-1500-2000-mg_3.1.1_mIr.root",
-          120.4, 3939077);
-        datasets.back().AddFile(datasetsDir + "QCD-Ht-2000-inf-mg_3.1.1_DTg.root",
-          25.25, 1981228 + 4016332);
-        datasets.back().AddFile(datasetsDir + "QCD-Ht-2000-inf-mg_ext1_3.1.1_Nlw.root",
-          25.25, 1981228 + 4016332);
+        // datasets.back().AddFile(datasetsDir + "QCD-Ht-100-200-mg_3.1.1_Kah.root",
+        //   27540000, 82095800);
+        datasets.back().AddFile(datasetsDir + "QCD-Ht-200-300-mg_3ac2b9b_ilj_p*.root",
+          1717000, 38816448);
+        datasets.back().AddFile(datasetsDir + "QCD-Ht-300-500-mg_3ac2b9b_AXa_p*.root",
+          351300, 16828258 + 37875602);
+        datasets.back().AddFile(datasetsDir + "QCD-Ht-300-500-ext1-mg_3ac2b9b_fMW_p*.root",
+          351300, 16828258 + 37875602);
+        datasets.back().AddFile(datasetsDir + "QCD-Ht-500-700-mg_3ac2b9b_ENN_p*.root",
+          31630, 18785349 + 44034159);
+        datasets.back().AddFile(datasetsDir + "QCD-Ht-500-700-ext1-mg_3ac2b9b_Nns_p*.root",
+          31630, 18785349 + 44034159);
+        datasets.back().AddFile(datasetsDir + "QCD-Ht-700-1000-mg_3ac2b9b_ROK_p*.root",
+          6802, 15621634 + 29832311);
+        datasets.back().AddFile(datasetsDir + "QCD-Ht-700-1000-ext1-mg_3ac2b9b_aTr_p*.root",
+          6802, 15621634 + 29832311);
+        datasets.back().AddFile(datasetsDir + "QCD-Ht-1000-1500-mg_3ac2b9b_IAf.root",
+          1206, 4889688 + 10327758);
+        datasets.back().AddFile(datasetsDir + "QCD-Ht-1000-1500-ext1-mg_3ac2b9b_CiT_p*.root",
+          1206, 4889688 + 10327758);
+        datasets.back().AddFile(datasetsDir + "QCD-Ht-1500-2000-mg_3ac2b9b_vmO.root",
+          120.4, 3928444);
+        datasets.back().AddFile(datasetsDir + "QCD-Ht-2000-inf-mg_3ac2b9b_hwT.root",
+          25.25, 1992472);
     }
     
     
@@ -125,17 +129,17 @@ int main(int argc, char **argv)
     // recoilBuilder->SetBetaPtFraction(0.05);
     manager.RegisterPlugin(recoilBuilder);
     
-    manager.RegisterPlugin(new DynamicTriggerFilter({{"PFJet140", 2.896}, {"PFJet200", 19.898},
-      {"PFJet260", 202.492}, {"PFJet320", 423.595}, {"PFJet400", 938.067},
-      {"PFJet450", 2312.360}}));
+    manager.RegisterPlugin(new DynamicTriggerFilter({{"PFJet140", 205.365}, {"PFJet200", 205.365},
+      {"PFJet260", 205.365}, {"PFJet320", 205.365}, {"PFJet400", 205.365},
+      {"PFJet450", 205.365}}));
     
     if (dataGroup != DatasetGroup::Data)
     {
         manager.RegisterPlugin(new PECPileUpReader);
-        manager.RegisterPlugin(new DynamicPileUpWeight({"pileup_Run2015CD_PFJet140_finebin.root",
-          "pileup_Run2015CD_PFJet200_finebin.root", "pileup_Run2015CD_PFJet260_finebin.root",
-          "pileup_Run2015CD_PFJet320_finebin.root", "pileup_Run2015CD_PFJet400_finebin.root",
-          "pileup_Run2015CD_PFJet450_finebin.root"}, "simPUProfiles_76X.root", 0.05));
+        manager.RegisterPlugin(new DynamicPileUpWeight({"pileup_Run2016B_v1_finebin.root",
+          "pileup_Run2016B_v1_finebin.root", "pileup_Run2016B_v1_finebin.root",
+          "pileup_Run2016B_v1_finebin.root", "pileup_Run2016B_v1_finebin.root",
+          "pileup_Run2016B_v1_finebin.root"}, "simPUProfiles_80X.root", 0.05));
     }
     
     manager.RegisterPlugin(new BalanceVars);
