14373596fa342c1da4ee2b420386cadc8e4c8f0f
Andrey Popov <rainfinder@gmail.com>
2017-03-09 12:36:50 +0100

    Add a plugin to store event weights
    For the moment do not drop the dataset weight from BalanceVars to allow
    old code to read new tuples.

---
+++ mensura/plugins/DumpWeights.cpp
@@ -0,0 +1,128 @@
+#include <DumpWeights.hpp>
+
+#include <mensura/core/Processor.hpp>
+#include <mensura/core/ROOTLock.hpp>
+
+#include <mensura/extensions/TFileService.hpp>
+#include <mensura/extensions/WeightCollector.hpp>
+
+#include <mensura/PECReader/PECGeneratorReader.hpp>
+
+#include <sstream>
+#include <stdexcept>
+
+
+using namespace std::string_literals;
+
+
+DumpWeights::DumpWeights(std::string const &name,
+  std::string const generatorPluginName_ /*= ""*/,
+  std::string const weightCollectorName_ /*= ""*/):
+    AnalysisPlugin(name),
+    fileServiceName("TFileService"), fileService(nullptr),
+    generatorPluginName(generatorPluginName_), generatorPlugin(nullptr),
+    weightCollectorName(weightCollectorName_), weightCollector(nullptr)
+{}
+
+
+DumpWeights::DumpWeights(DumpWeights const &src):
+    AnalysisPlugin(src),
+    fileServiceName(src.fileServiceName), fileService(nullptr),
+    generatorPluginName(src.generatorPluginName), generatorPlugin(nullptr),
+    weightCollectorName(src.weightCollectorName), weightCollector(nullptr),
+    systWeights(src.systWeights)
+{}
+
+
+void DumpWeights::BeginRun(Dataset const &dataset)
+{
+    if (not dataset.IsMC())
+    {
+        std::ostringstream message;
+        message << "DumpWeights[\"" << GetName() << "\"]::BeginRun: The current dataset is data, "
+          "but this plugin should only be used with simulation.";
+        throw std::runtime_error(message.str());
+    }
+    
+    
+    // Save pointers to services and other plugins
+    fileService = dynamic_cast<TFileService const *>(GetMaster().GetService(fileServiceName));
+    
+    if (generatorPluginName != "")
+        generatorPlugin =
+          dynamic_cast<PECGeneratorReader const *>(GetDependencyPlugin(generatorPluginName));
+    
+    if (weightCollectorName != "")
+        weightCollector =
+          dynamic_cast<WeightCollector const *>(GetDependencyPlugin(weightCollectorName));
+    
+    
+    // Adjust the size of the array to store alternative weights
+    unsigned nSystWeights = 0;
+    
+    if (weightCollectorName != "")
+        for (unsigned iPlugin = 0; iPlugin < weightCollector->GetNumPlugins(); ++iPlugin)
+            nSystWeights += 2 * weightCollector->GetPlugin(iPlugin)->GetNumVariations();
+    
+    systWeights.resize(nSystWeights);
+    
+    
+    // Create output tree
+    tree = fileService->Create<TTree>("", "Weights", "Nominal and alternative weights");
+    
+    
+    // Assign branch addresses
+    ROOTLock::Lock();
+    
+    tree->Branch("weight", &weight);
+    tree->Branch("systWeights", systWeights.data(),
+      ("systWeights["s + std::to_string(systWeights.size()) + "]/F").c_str());
+    
+    ROOTLock::Unlock();
+    
+    
+    // Common event weight in this dataset
+    auto const &firstFile = dataset.GetFiles().front();
+    weightDataset = firstFile.GetWeight();
+}
+
+
+Plugin *DumpWeights::Clone() const
+{
+    return new DumpWeights(*this);
+}
+
+
+bool DumpWeights::ProcessEvent()
+{
+    double w = weightDataset;
+    
+    if (generatorPlugin)
+        w *= generatorPlugin->GetNominalWeight();
+    
+    
+    if (weightCollector)
+    {
+        weight = w * weightCollector->GetWeight();
+        unsigned curWeightIndex = 0;
+        
+        for (unsigned iPlugin = 0; iPlugin < weightCollector->GetNumPlugins(); ++iPlugin)
+        {
+            EventWeightPlugin const *plugin = weightCollector->GetPlugin(iPlugin);
+            
+            for (unsigned iVar = 0; iVar < plugin->GetNumVariations(); ++iVar)
+            {
+                systWeights.at(curWeightIndex) = w * weightCollector->GetWeightUp(iPlugin, iVar);
+                systWeights.at(curWeightIndex + 1) =
+                  w * weightCollector->GetWeightDown(iPlugin, iVar);
+                curWeightIndex += 2;
+            }
+        }
+    }
+    else
+        weight = w;
+    
+    
+    tree->Fill();
+    return true;
+}
---
+++ mensura/plugins/DumpWeights.hpp
@@ -0,0 +1,110 @@
+#pragma once
+
+#include <mensura/core/AnalysisPlugin.hpp>
+
+#include <TTree.h>
+
+#include <string>
+#include <vector>
+
+
+class PECGeneratorReader;
+class TFileService;
+class WeightCollector;
+
+
+/**
+ * \class DumpWeights
+ * \brief A plugin to save event weights
+ * 
+ * Creates a TTree with nominal event weight and alternative weights that account for systematic
+ * variations. The weights are read from a WeightCollector, and all provided systematic variations
+ * are evaluated. In addition to the WeightCollector, this plugin reads the nominal generator-level
+ * weight (if a PECGeneratorReader is provided), which is incorporated into all stored weights. The
+ * weight due to cross section and number of events is always included.
+ * 
+ * Nominal and alternative weights are written into two different branches. The alternative weights
+ * are stored as an array, whose size is equal to the number of variations provided by the
+ * WeightCollector and thus might depend on the dataset.
+ * 
+ * This plugin should only be used with simulation.
+ */
+class DumpWeights: public AnalysisPlugin
+{
+public:
+    /**
+     * \brief Constructor
+     * 
+     * The arguments are the name for the new plugin and names of a PECGeneratorReader and a
+     * WeightCollector. If any of the latter two are empty, the corresponding weights are not
+     * evaluated.
+     */
+    DumpWeights(std::string const &name, std::string const generatorPluginName = "",
+      std::string const weightCollectorName = "");
+    
+    /// Default move constructor
+    DumpWeights(DumpWeights &&) = default;
+    
+    /// Assignment operator is deleted
+    DumpWeights &operator=(DumpWeights const &) = delete;
+    
+private:
+    /// Copy constructor
+    DumpWeights(DumpWeights const &src);
+
+public:
+    /**
+     * \brief Saves pointers to dependencies and sets up the output tree
+     * 
+     * Reimplemented from Plugin.
+     */
+    virtual void BeginRun(Dataset const &dataset) override;
+    
+    /**
+     * \brief Creates a newly configured clone
+     * 
+     * Implemented from Plugin.
+     */
+    virtual Plugin *Clone() const override;
+
+private:
+    /**
+     * \brief Saves event weights
+     * 
+     * Implemented from Plugin.
+     */
+    virtual bool ProcessEvent() override;
+
+private:
+    /// Name of TFileService
+    std::string fileServiceName;
+    
+    /// Non-owning pointer to TFileService
+    TFileService const *fileService;
+    
+    /// Name of plugin that provides generator-level weight
+    std::string generatorPluginName;
+    
+    /// Non-owning pointer to plugin that provides generator-level weight
+    PECGeneratorReader const *generatorPlugin;
+    
+    /// Name of a plugin that collects event weights
+    std::string weightCollectorName;
+    
+    /// Non-owning pointer to a plugin that collects event weights
+    WeightCollector const *weightCollector;
+    
+    /**
+     * \brief Common event weight for the current dataset
+     * 
+     * Equals cross section divided by the number of events in the dataset.
+     */
+    double weightDataset;
+    
+    /// Non-owning pointer to output tree
+    TTree *tree;
+    
+    // Output buffers
+    Float_t weight;
+    std::vector<Float_t> systWeights;
+};
